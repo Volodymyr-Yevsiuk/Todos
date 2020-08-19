236be4aa7a6678433092c69349903eb1213abbe1
Volodymyr <vov2706@gmail.com>
2020-08-19 22:07:18 +0300

    Updates

---
+++ precache-manifest.b23f2e27fe24c99158df7c8ed23d94bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba388e8543c980c4d5d0c0183313906b",
+    "url": "/Volodymyr-Yevsiuk/Todos/index.html"
+  },
+  {
+    "revision": "953ef251e596e8504dea",
+    "url": "/Volodymyr-Yevsiuk/Todos/static/css/main.6086e908.chunk.css"
+  },
+  {
+    "revision": "fb8a4e35e1bfa13f4d06",
+    "url": "/Volodymyr-Yevsiuk/Todos/static/js/2.60c56765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Volodymyr-Yevsiuk/Todos/static/js/2.60c56765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953ef251e596e8504dea",
+    "url": "/Volodymyr-Yevsiuk/Todos/static/js/main.a2139c75.chunk.js"
+  },
+  {
+    "revision": "4cbbaf1465abd2454c7e",
+    "url": "/Volodymyr-Yevsiuk/Todos/static/js/runtime-main.6dd4f92c.js"
+  }
+]);
